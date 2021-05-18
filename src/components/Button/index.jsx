e757405c533f2f7bca059e9aaaef1b4830a1e364
devHudi <devhudi@gmail.com>
2021-05-18 21:48:54 +0900

    feat: implement Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #212529;
+  border: none;
+  font-size: 12pt;
+  color: #f8f9fa;
+`;
+
+export default Button;
