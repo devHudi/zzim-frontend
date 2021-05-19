483da044189ac1d7b2941d7be773c753a20f6c84
devHudi <devhudi@gmail.com>
2021-05-19 22:08:31 +0900

    feat: change button padding

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  padding: 10px 20px;
+  padding: 15px 20px;
   background-color: #212529;
   border: none;
   font-size: 12pt;
