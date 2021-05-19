4953f78f25cf13ba1a98e421b7be62477fb1357d
devHudi <devhudi@gmail.com>
2021-05-19 22:03:43 +0900

    feat: change global style

--- src/components/GlobalStyles/index.jsx
+++ src/components/GlobalStyles/index.jsx
@@ -4,6 +4,10 @@ import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  html, body, #root {
+    height: 100%;
+  }
+
   * {
     font-family: 'Noto Sans KR', sans-serif;
   }
