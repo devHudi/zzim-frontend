c7ae4c5b11222cc2e88fa3d503bf09ed3c3e005d
devHudi <devhudi@gmail.com>
2021-05-19 21:26:14 +0900

    feat: implement BackButton

---
+++ src/components/BackButton/index.jsx
@@ -0,0 +1,31 @@
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { RiArrowLeftSLine } from "react-icons/ri";
+
+const Icon = styled.span`
+  cursor: pointer;
+  font-size: 28px;
+  color: ${(props) => (props.white ? "#ffffff" : "#adb5bd")};
+  filter: drop-shadow(0 0 2px rgba(0, 0, 0, 0.3));
+  transition: color 0.3s;
+
+  &:hover {
+    color: ${(props) => (props.white ? "#f1f3f5" : "#868e96")};
+  }
+`;
+
+const BackButton = ({ white }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.goBack();
+  };
+
+  return (
+    <Icon onClick={handleClick} white={white}>
+      <RiArrowLeftSLine />
+    </Icon>
+  );
+};
+
+export default BackButton;
