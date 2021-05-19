04fb2b555d9a0276227bc07c5a7d679791aba1cc
devHudi <devhudi@gmail.com>
2021-05-19 21:46:47 +0900

    feat: add overflow style into Typography

--- src/components/Typography/index.jsx
+++ src/components/Typography/index.jsx
@@ -7,6 +7,9 @@ const CommonTypography = styled.div`
   ${(props) => props.green && "color: #2f9e44;"}
   ${(props) => props.blue && "color: #1971c2;"}
   ${(props) => props.pointer && "cursor: pointer;"}
+  
+  overflow:hidden;
+  text-overflow: ellipsis;
 `;
 
 Typography.FormTitle = styled(CommonTypography)`
