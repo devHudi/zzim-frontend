8d0111995605a406a3719c747398d3404fc6d269
devHudi <devhudi@gmail.com>
2021-05-22 09:45:26 +0900

    feat: improve ItemDetail

--- src/pages/ItemDetail/index.jsx
+++ src/pages/ItemDetail/index.jsx
@@ -10,22 +10,52 @@ import {
   MeatBallsMenu,
   BackButton,
   Image,
-  Typography,
+  Button,
   Space,
   MallLogo,
   TextField,
 } from "components";
-
-const BackWrapper = styled.div`
-  position: absolute;
-  top: 10px;
-  left: 10px;
-`;
+import ButtonGroup from "components/ButtonGroup";
 
 const MenuWrapper = styled.div`
-  position: absolute;
-  top: 10px;
-  right: 10px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TitleWrapper = styled.div`
+  padding: 20px;
+`;
+
+const ItemName = styled.h1`
+  font-weight: bold;
+  font-size: 20pt;
+  line-height: 1.2;
+`;
+
+const ItemPrice = styled.div`
+  font-size: 15pt;
+  color: #868e96;
+`;
+
+const FlexBox = styled.div`
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DetailTitle = styled.span`
+  font-size: 11pt;
+  font-weight: bold;
+`;
+
+const DetailDesc = styled.span`
+  max-width: 70%;
+  font-size: 11pt;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: #343a40;
 `;
 
 const ItemDetail = () => {
@@ -52,65 +82,64 @@ const ItemDetail = () => {
     history.push("/");
   };
 
+  const handleOutLink = () => {
+    window.open("http://www.naver.com");
+  };
+
   return (
     <>
-      <Image src="https://s3.ap-northeast-2.amazonaws.com/productmain/20210417_637542253839755265_3019034_0.jpg">
-        <BackWrapper>
-          <BackButton />
-        </BackWrapper>
-        <MenuWrapper>
-          <MeatBallsMenu left>
-            <MeatBallsMenu.Menu onClick={handleModify} blue={modify}>
-              {modify ? "수정완료" : "수정하기"}
-            </MeatBallsMenu.Menu>
-            <MeatBallsMenu.Menu onClick={handlePurchase}>
-              구매완료
-            </MeatBallsMenu.Menu>
-            <MeatBallsMenu.Menu onClick={handleDelete} red>
-              삭제하기
-            </MeatBallsMenu.Menu>
-          </MeatBallsMenu>
-        </MenuWrapper>
-      </Image>
+      <MenuWrapper>
+        <BackButton />
+        <MeatBallsMenu left>
+          <MeatBallsMenu.Menu onClick={handleModify} blue={modify}>
+            {modify ? "수정완료" : "수정하기"}
+          </MeatBallsMenu.Menu>
+          <MeatBallsMenu.Menu onClick={handlePurchase}>
+            구매완료
+          </MeatBallsMenu.Menu>
+          <MeatBallsMenu.Menu onClick={handleDelete} red>
+            삭제하기
+          </MeatBallsMenu.Menu>
+        </MeatBallsMenu>
+      </MenuWrapper>
+
+      <TitleWrapper>
+        {modify ? (
+          <TextField />
+        ) : (
+          <ItemName>메종키츠네 더블 폭스헤드 반팔 티셔츠 BU0</ItemName>
+        )}
+        <Space />
+        {modify ? <TextField /> : <ItemPrice>{comma(35000)} 원</ItemPrice>}
+        <Space size="20" />
+      </TitleWrapper>
+
+      <Image src="https://image.a-land.co.kr/data/aland_data/images/product/11/00/10/53/37/b_1100105337.gif"></Image>
+      <Space size="20" />
+
       <PagePadding>
-        {
-          // waiting for TextField component
-        }
-        <Typography.FormTitle>상품명</Typography.FormTitle>
-        {modify ? (
-          <TextField></TextField>
-        ) : (
-          <Typography.FormValue>예쁜반팔</Typography.FormValue>
-        )}
-        <Space />
-        <Typography.FormTitle>가격</Typography.FormTitle>{" "}
-        {modify ? (
-          <TextField></TextField>
-        ) : (
-          <Typography.FormValue>{comma(35000)} 원</Typography.FormValue>
-        )}
-        <Space />
-        <Typography.FormTitle>배송비</Typography.FormTitle>
-        {modify ? (
-          <TextField></TextField>
-        ) : (
-          <Typography.FormValue>{comma(2500)} 원</Typography.FormValue>
-        )}
-        <Space />
-        <Typography.FormTitle>쇼핑몰</Typography.FormTitle>
-        <Typography.FormValue blue pointer>
-          <MallLogo mall="coupang" />
-        </Typography.FormValue>
-        <Space />
-        <Typography.FormTitle>상품 URL</Typography.FormTitle>
-        <Typography.FormValue blue pointer>
-          https://www.naver.com
-        </Typography.FormValue>
-        <Space />
-        <Typography.FormTitle>담은 날짜</Typography.FormTitle>
-        <Typography.FormValue>
-          {moment(new Date()).format("YYYY년 MM월 DD일")}
-        </Typography.FormValue>
+        <FlexBox>
+          <DetailTitle>배송비</DetailTitle>
+          {modify ? <TextField /> : <DetailDesc>{comma(2500)} 원</DetailDesc>}
+        </FlexBox>
+
+        <FlexBox>
+          <DetailTitle>쇼핑몰</DetailTitle>
+          <MallLogo mall="coupang" height="15px" />
+        </FlexBox>
+
+        <FlexBox>
+          <DetailTitle>담은 날짜</DetailTitle>
+          <DetailDesc>
+            {moment(new Date()).format("YYYY년 MM월 DD일")}
+          </DetailDesc>
+        </FlexBox>
+
+        <Space size="20" />
+
+        <ButtonGroup>
+          <Button onClick={handleOutLink}> 상품 바로가기</Button>
+        </ButtonGroup>
       </PagePadding>
     </>
   );
