dd355f40bd40aea8a8aa48e29707c8f69f9518bf
devHudi <devhudi@gmail.com>
2021-05-22 09:45:07 +0900

    feat: add height option into MallImage

--- src/components/MallLogo/index.jsx
+++ src/components/MallLogo/index.jsx
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import coupang from "./imgs/coupang.png";
 
 const Image = styled.img`
-  height: 25px;
+  height: ${(props) => props.height || "25px"};
 `;
 
 const mallList = {
   coupang,
 };
 
-const MallLogo = ({ mall }) => {
-  return <Image src={mallList[mall]} />;
+const MallLogo = ({ mall, height }) => {
+  return <Image src={mallList[mall]} height={height} />;
 };
 
 export default MallLogo;
